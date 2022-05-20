55a3ab44191d24d44f31ae0f8ae9bd1d29bc35a2
Connie Standish <conniestand@gmail.com>
2022-05-20 16:27:45 -0400

    added formulas to calculate age

--- index.js
+++ index.js
@@ -5,8 +5,18 @@ function greet() {
     alert("Hello " + nameOfUser)
     var ageOfUser = parseInt(prompt('How old are you?'))
     var birthDay = confirm('Have you had your birthday yet?')
-    
-    
 
+    var birthDate = new Date()
+    console.log(birthDate.getFullYear())
+
+    var yesBirth = birthDate.getFullYear() - ageOfUser
+    var noBirth = birthDate.getFullYear() - ageOfUser + 1
+
+    if (birthDay == false) {
+        alert(noBirth)
+    } else {
+        alert(yesBirth)
+    }
+    
     
 }
