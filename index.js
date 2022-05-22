1c2064de23a908f19335a8f3a93d6b05148d505c
Connie Standish <conniestand@gmail.com>
2022-05-22 17:23:36 -0400

    attempt to troubleshoot if else statement

--- index.js
+++ index.js
@@ -3,23 +3,26 @@
 function greet() {
     var nameOfUser = prompt('What is your name?')
     alert("Hello " + nameOfUser)
-    if (nameOfUser === null) {
-        return;
-    }
+    if (nameOfUser === null) return
     
     var ageOfUser = parseInt(prompt('How old are you?'))
+    
     var birthDay = confirm('Have you had your birthday yet?')
+    console.log(typeof birthDay)
 
     var birthDate = new Date()
     // console.log(birthDate.getFullYear())
 
     var yesBirth = birthDate.getFullYear() - ageOfUser
+    // console.log(Boolean(yesBirth))
+    
     var noBirth = birthDate.getFullYear() - ageOfUser + 1
+    // console.log(!noBirth)
 
     if (birthDay === false) {
-        alert("Your birth year has NOT yet passed, you were born " + noBirth)
+        alert("Your birthday has NOT passed, you were born " + noBirth)
     } else {
-        alert("Your birth year has passed, you were born " + yesBirth)
+        alert("Your birthday has passed, you were born " + yesBirth)
     }
     
     
