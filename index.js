1bec452eaaea3b168be29d0dd89f3fcc3d91376d
Connie Standish <conniestand@gmail.com>
2022-05-22 13:19:52 -0400

    Attempted to troubleshoot birthday alerts and return name of user function if user presses cancel

--- index.js
+++ index.js
@@ -3,19 +3,24 @@
 function greet() {
     var nameOfUser = prompt('What is your name?')
     alert("Hello " + nameOfUser)
+    if (nameOfUser === null) {
+        return;
+    }
+    
+
     var ageOfUser = parseInt(prompt('How old are you?'))
     var birthDay = confirm('Have you had your birthday yet?')
 
     var birthDate = new Date()
-    console.log(birthDate.getFullYear())
+    // console.log(birthDate.getFullYear())
 
     var yesBirth = birthDate.getFullYear() - ageOfUser
     var noBirth = birthDate.getFullYear() - ageOfUser + 1
 
-    if (birthDay == false) {
-        alert(noBirth)
+    if (birthDay === false) {
+        alert("Your birth year has NOT yet passed, you were born " + noBirth)
     } else {
-        alert(yesBirth)
+        alert("Your birth year has passed, you were born " + yesBirth)
     }
     
     
